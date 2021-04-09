1c8d3a8f323dc9a74240312850b4f5bff1b9d331
jcnh74 <hanusek@gmail.com>
2021-04-09 15:57:14 -0400

    update to package.json and functions.php

--- functions.php
+++ functions.php
@@ -96,6 +96,16 @@ function setEnvironment() {
 }
 add_action( 'after_setup_theme', 'setEnvironment' );
 
+//Page Slug Body Class
+function add_slug_body_class( $classes ) {
+  global $post;
+  if ( isset( $post ) ) {
+    $classes[] = $post->post_type . '-' . $post->post_name;
+  }
+  return $classes;
+}
+add_filter( 'body_class', 'add_slug_body_class' );
+
 
 
 
